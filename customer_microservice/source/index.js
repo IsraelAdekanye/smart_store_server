6cdab802e56e353606058b0173a0db4ad8709e90
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-08 19:15:54 +0100

    Added Server Entry point index.js

--- customer_microservice/source/index.js
+++ customer_microservice/source/index.js
@@ -1,4 +1,32 @@
 const express = require('express');
+const cors  = require('cors');
 const { PORT } = require('./config');
 const { databaseConnection } = require('./database');
-const { CreateChannel } = require('./utils')
+const { CreateChannel } = require('./utils');
+
+const StartServer = async() => {
+
+    const app = express();
+    
+    await databaseConnection();
+
+    const channel = await CreateChannel()
+
+    await expressApp(app, channel);
+    
+
+    app.listen(PORT, () => {
+          console.log(`listening to port ${PORT}`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    })
+    .on('close', () => {
+        channel.close();
+    })
+    
+
+}
+
+StartServer();
