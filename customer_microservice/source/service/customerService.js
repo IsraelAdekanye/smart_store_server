da93bb8141216baa8c8b7f9b5c34e7e3d90e3ce0
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-10 21:58:19 +0100

    Added customer service controller

--- customer_microservice/source/service/customerService.js
+++ customer_microservice/source/service/customerService.js
@@ -96,5 +96,23 @@ class CustomerService {
         const { event, data } =  payload;
 
         const { userId, product, order, qty } = data;
+
+        switch(event){
+            case 'ADD_TO_WISHLIST':
+            case 'REMOVE_FROM_WISHLIST':
+                this.AddToWishlist(userId,product)
+                break;
+            case 'ADD_TO_CART':
+                this.ManageCart(userId,product, qty, false);
+                break;
+            case 'REMOVE_FROM_CART':
+                this.ManageCart(userId,product,qty, true);
+                break;
+            case 'CREATE_ORDER':
+                this.ManageOrder(userId,order);
+                break;
+            default:
+                break;
+        }
     }
 }
