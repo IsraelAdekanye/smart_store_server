e544188b868996a15b2fe888774a990c1e9fa4fa
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-13 16:40:14 +0100

    Added API Entry point

--- customer_microservice/source/api/customer.js
+++ customer_microservice/source/api/customer.js
@@ -2,3 +2,31 @@ const CustomerService = require('../service/customerService');
 const  UserAuth = require('./middlewares/auth');
 const { SubscribeMessage } = require('../utils');
 
+module.exports = (app, channel) => {
+    
+    const service = new CustomerService();
+
+    SubscribeMessage(channel, service);
+
+
+    app.post('/signup', async (req,res,next) => {
+        const { email, password, phone } = req.body;
+        const { data } = await service.SignUp({ email, password, phone}); 
+        res.json(data);
+
+    });
+
+    
+
+    
+     
+
+    
+     
+
+    
+    
+    
+
+    
+}
