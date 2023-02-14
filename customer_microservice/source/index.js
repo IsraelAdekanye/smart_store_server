485cd529a5d9cfbf0bd30d0ca2d03259a624fb9f
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-14 21:14:50 +0100

    Added API Entry point

--- customer_microservice/source/index.js
+++ customer_microservice/source/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const cors  = require('cors');
 const { PORT } = require('./config');
-const { databaseConnection } = require('./database');
+const { databaseConnection } = require('./db/customerDatabase');
 const customer = require('./api/customer');
 const { CreateChannel } = require('./utils');
 
 
-app.use(cors());
 
-app.use(express.json());
-
-app.use(express.static(__dirname + '/public'))
 
 const StartServer = async() => {
 
     const app = express();
     
+    app.use(cors());
+
+    app.use(express.json());
+
+    app.use(express.static(__dirname + '/public'))
     await databaseConnection();
 
     // const channel = await CreateChannel()
