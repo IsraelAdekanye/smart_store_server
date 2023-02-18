c9762e41cc7d406a885b263c166127ff45c406d2
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-18 22:04:08 +0100

    Added Products Microservice

--- products_microservice/source/index.js
+++ products_microservice/source/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const cors = require("cors");
+const { PRODUCT_PORT } = require('./config');
+const { databaseConnection } = require('./database');
+const { products } = require("./api");
 
-const app = express();
+const { CreateChannel } = require("./utils");
 
-app.use(express.json());
+const StartServer = async() => {
 
-app.use('/', (req,res,next) => {
+    const app = express();
 
-    return res.status(200).json({"msg": "Hello from Products"})
-})
+    app.use(express.json());
+    app.use(cors());
+    app.use(express.static(__dirname + "/public"));
 
+    const channel = await CreateChannel();
 
-app.listen(8002, () => {
-    console.log('Products is Listening to Port 8002')
-})
+    products(app, channel);
+    
+    await databaseConnection();
+
+    app.listen(PORT, () => {
+        console.log(`listening to port ${PRODUCT_PORT}`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    })
+
+}
+
+StartServer();
