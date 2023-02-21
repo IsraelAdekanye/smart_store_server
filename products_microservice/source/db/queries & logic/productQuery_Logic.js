c042230db85af3cca55413288fa618d8b3e8263c
IsraelMacBook <israeladedigba@yahoo.com>
2023-02-21 21:06:03 +0100

    Added Products Microservice

---
+++ products_microservice/source/db/queries & logic/productQuery_Logic.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { ProductModel } = require("../models");
+
+//Dealing with data base operations
+class ProductRepository {
+
+
+ 
+
+
+
+
+
+
+    
+}
+
+module.exports = ProductRepository;
